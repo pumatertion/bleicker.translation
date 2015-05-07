0719b2ecb13499a51911556102f3c9874167770a
Carsten Bleicker <carsten@bleicker.de>
2015-05-07 16:59:16 +0200

    Update TranslateTrait

--- src/AbstractTranslate.php
+++ src/AbstractTranslate.php
@@ -2,11 +2,8 @@
 
 namespace Bleicker\Translation;
 
-use Bleicker\Translation\Exception\PropertyDoesNotExistsException;
-use Bleicker\Translation\Exception\TranslationAlreadyExistsException;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\Criteria;
 
 /**
  * Class AbstractTranslate
@@ -15,7 +12,7 @@ use Doctrine\Common\Collections\Criteria;
  */
 abstract class AbstractTranslate implements TranslateInterface {
 
-	//use TranslateTrait;
+	use TranslateTrait;
 
 	/**
 	 * @var Collection
@@ -24,64 +21,5 @@ abstract class AbstractTranslate implements TranslateInterface {
 
 	public function __construct() {
 		$this->translations = new ArrayCollection();
-	}
-
-	/**
-	 * @param TranslationInterface $translation
-	 * @return $this
-	 */
-	public function removeTranslation(TranslationInterface $translation) {
-		if ($this->hasTranslation($translation)) {
-			$this->translations->removeElement($this->getTranslation($translation));
-		}
-		return $this;
-	}
-
-	/**
-	 * @param TranslationInterface $translation
-	 * @return $this
-	 * @throws TranslationAlreadyExistsException
-	 * @throws PropertyDoesNotExistsException
-	 */
-	public function addTranslation(TranslationInterface $translation) {
-		if ($this->hasTranslation($translation)) {
-			throw new TranslationAlreadyExistsException('Translation "' . (string)$translation . '" already exists', 1431005644);
-		}
-		$this->translations->add($translation);
-		return $this;
-	}
-
-	/**
-	 * @param TranslationInterface $translation
-	 * @return boolean
-	 */
-	public function hasTranslation(TranslationInterface $translation) {
-		$expr = Criteria::expr();
-		$criteria = Criteria::create();
-		$criteria->andWhere(
-			$expr->andX(
-				$expr->eq('propertyName', $translation->getPropertyName()),
-				$expr->eq('locale', $translation->getLocale())
-			)
-		);
-		$matchingTranslations = $this->translations->matching($criteria);
-		return (boolean)$matchingTranslations->count();
-	}
-
-	/**
-	 * @param TranslationInterface $translation
-	 * @return TranslationInterface
-	 */
-	public function getTranslation(TranslationInterface $translation) {
-		$expr = Criteria::expr();
-		$criteria = Criteria::create();
-		$criteria->andWhere(
-			$expr->andX(
-				$expr->eq('propertyName', $translation->getPropertyName()),
-				$expr->eq('locale', $translation->getLocale())
-			)
-		);
-		$matchingTranslations = $this->translations->matching($criteria);
-		return $matchingTranslations->first();
 	}
 }
